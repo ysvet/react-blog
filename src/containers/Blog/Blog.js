7a9e9aa0296a9f3666c74d8bb88cb2ee5f98a025
ysvet <iurie.shvets@gmail.com>
2019-04-17 21:25:01 +0300

    axios handling errors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount () {
@@ -24,6 +25,9 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
                 // console.log(response);
+            })
+            .catch(error => {
+                this.setState({error: true});
             });
     } 
 
@@ -32,15 +36,18 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                 key={post.id} 
-                 title={post.title}
-                 author={post.author} 
-                 clicked={() => this.postSelectedHandler(post.id)}/>
-        });
-
-        return (
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                     key={post.id} 
+                     title={post.title}
+                     author={post.author} 
+                     clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+    
+        }
+          return (
             <div>
                 <section className="Posts">
                     {posts}
