438ea85687f0fe6dfa779629aa1ff70442856fea
ysvet <iurie.shvets@gmail.com>
2019-04-18 20:16:46 +0300

    axios global defaults

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+// demonstration only:
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
