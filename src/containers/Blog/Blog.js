3e063b7ba6ee80227270548fce930f784e640dce
ysvet <iurie.shvets@gmail.com>
2019-04-23 20:19:35 +0300

    links between the pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
 import './Blog.css';
 
 class Blog extends Component {
@@ -17,6 +19,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path='/' exact component={Posts} />
+                <Route path='/new-post' exact component={NewPost} />
             </div>
         );
     }
