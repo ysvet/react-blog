7ecaf7e6019c56cd89a34f00d0a3a5788c48ba9a
ysvet <iurie.shvets@gmail.com>
2019-04-29 10:24:49 +0300

    nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 import './Blog.css';
 
@@ -15,13 +14,13 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                                 to="/" 
+                                 to="/posts/" 
                                  exact
                                  activeClassName="mmy-active"
                                  activeStyle={{
                                      color: '#fa923f',
                                      textDecoration: 'underline'   
-                                 }}>Home</NavLink></li>
+                                 }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -30,10 +29,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path='/' exact component={Posts} />
                 <Switch>
-                    <Route path='/new-post' exact component={NewPost} />
-                    <Route path='/:id' exact component={FullPost} />
+                    <Route path='/new-post' component={NewPost} />
+                    <Route path='/posts'  component={Posts} />
                 </Switch>
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
+import FullPost from '../FullPost/FullPost';
 
 import './Posts.css';
 
@@ -40,7 +41,7 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id} >
+                    <Link to={'/posts/' + post.id} key={post.id} >
                         <Post 
                             title={post.title} 
                             author={post.author}
@@ -50,9 +51,13 @@ class Posts extends Component {
         }
 
         return (
-            <section className="Posts">
-                 {posts}
-             </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                 </section>
+                 <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
+
         );
     }
 }
